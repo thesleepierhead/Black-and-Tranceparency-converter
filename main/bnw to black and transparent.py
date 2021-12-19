2d76f60ad49a51e9133e770c7d7d080a8d1174e5
SleepyHead <liweichugastone@gmail.com>
2021-12-19 16:48:55 +0000

    fix color profile issue and added comments

--- main/bnw to black and transparent.py
+++ main/bnw to black and transparent.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageTk
+from PIL import Image
 import tkinter as tk
 from tkinter import filedialog as fd 
 
@@ -15,10 +15,12 @@ class BnT:
         mnf.columnconfigure(1, weight=4, minsize=225)
         mnf.rowconfigure(0, weight=1)      
 
+        #setting global variables
         self.path = tk.StringVar()
         self.status = tk.StringVar()
         self.path_short = tk.StringVar()
 
+        #Info Labels
         tk.Label(mnf, text ='   ').grid(column=0, row=0)
         tk.Label(mnf, text='Black and Transparency converter:', font=('Default', 15)).grid(column=0, row=1, columnspan=2)
         tk.Label(mnf, text = 
@@ -26,11 +28,14 @@ class BnT:
             'and then converts it to an image made only of black pixels \n'
             ' of varying opacity.', relief = 'sunken').grid(column = 0, row = 2, columnspan = 2, pady = 10)
 
+
+        #Open File 
         tk.Button(mnf, text='Open a File',
             command=self.select_file).grid(column=0, row=3, pady = 10, sticky='E')
         
         tk.Label(mnf, textvariable = self.path_short, wraplength=260).grid(column=1, row=3, pady = 10, sticky='W')
 
+        #Save File
         tk.Button(mnf, text='Save', command=self.process).grid(column = 0, row = 4, pady = 10, sticky='E')
         tk.Label(mnf, textvariable = self.status).grid(column=1, row=4, pady = 10, sticky='W')
 
@@ -52,6 +57,7 @@ class BnT:
         
 
     def process(self):
+
         img_path = self.path.get()
         print(img_path)
         img = Image.open(img_path)
@@ -59,12 +65,13 @@ class BnT:
         datas = la.getdata()
         newData = []
         for item in datas:
-            newData.append((0, 255-item[0]))
+            newData.append((0, 255-item[0])) #pixel conversion
         la.putdata(newData)
+        out = la.convert('RGBA')
         
         file = fd.asksaveasfile(mode='wb', defaultextension=".png", filetypes=(("PNG file", "*.png"),("All Files", "*.*") ))
         if file:
-            la.save(file)
+            out.save(file)
             self.status.set("Done!")
 
 root = tk.Tk()
